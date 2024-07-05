22f987f4eaeeec1437e7cbddf97ed18ef8b4f329
kristechhub <kmj.algario@gmail.com>
2024-07-05 17:14:03 +0800

    udpated client url on backend

--- backend/Utilities/corsOptions.js
+++ backend/Utilities/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: 'http://localhost:5173', //URL to allow access
+    origin: process.env.CLIENT_URL, //URL to allow access
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", //methdos to use
     credentials: true, // Allow credentials (cookies) to be sent
 };
